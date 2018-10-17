a885c8086005abd71962ce1e877385a6d145521a
ehchoi <ehchoi@cafe24corp.com>
2018-10-17 18:03:14 +0900

    Initial commit

---
+++ src/main/java/com/silverhyuk/spring_thread_pool/SpringThreadPoolApplication.java
@@ -0,0 +1,12 @@
+package com.silverhyuk.spring_thread_pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThreadPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThreadPoolApplication.class, args);
+	}
+}
